0164c1dfb10232ee8a7e4ca1dc6dd9844ae3a03c
Mohana Misra <iammohanamisra@gmail.com>
2024-12-18 11:51:34 +0530

    Chained axios request instead of storing in a variable

--- src/api/disasters.js
+++ src/api/disasters.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const BASE_URL = "https://api.reliefweb.int/v1";
 
 export const fetchDisasters = async() => {
-    const response = await axios.get(`${BASE_URL}/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1&query%5Bvalue%5D=country.id%3A119&query%5Boperator%5D=AND`);
-    console.log(response.data);
+    await axios.get(`${BASE_URL}/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1&query%5Bvalue%5D=country.id%3A119&query%5Boperator%5D=AND`)
+        .then(response => {
+            console.log(response.data);
+        })
 }
