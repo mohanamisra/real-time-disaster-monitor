7709e009551b9792e5c3c25ed0d057348d7dfb2a
Mohana Misra <iammohanamisra@gmail.com>
2024-12-18 12:05:37 +0530

    API call successful

--- src/api/disasters.js
+++ src/api/disasters.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 const BASE_URL = "https://api.reliefweb.int/v1";
 
 export const fetchDisasters = async() => {
-    await axios.get(`${BASE_URL}/disasters?appname=rwint-user-0&profile=list&preset=latest&slim=1&query%5Bvalue%5D=country.id%3A119&query%5Boperator%5D=AND`)
-        .then(response => {
-            console.log(response.data);
-        })
+    return await axios
+        .get(`${BASE_URL}/disasters?appname=iammohanamisra&profile=list&preset=latest&slim=1&query%5Bvalue%5D=country.id%3A119&query%5Boperator%5D=AND`);
+}
+
+export default {
+    fetchDisasters,
 }
