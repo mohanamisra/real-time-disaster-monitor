1c1fa10ee8d412461f963dce6d0b515c9b7a1508
Mohana Misra <iammohanamisra@gmail.com>
2024-12-19 10:43:41 +0530

    Added content scaffolding for the sidebar section with general overview and helping out sections

---
+++ src/tests/disasters.test.js
@@ -0,0 +1,8 @@
+import {fetchAllDisasters, fetchOngoingDisasters} from '../api/disasters.js';
+import axios from 'axios';
+import {jest} from "globals";
+
+jest.mock('axios');
+
+describe('ReliefWeb API Calls', () => {
+})
