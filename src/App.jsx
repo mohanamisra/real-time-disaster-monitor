3185bc9a561c85d0cf853c6ddda55ae1ba410a55
Mohana Misra <iammohanamisra@gmail.com>
2024-12-19 13:17:31 +0530

    Added AI damage assessment tool usage instructions

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,13 @@ function App() {
                         </div>
                         <div className="ai-damage-assement">
                             <h2>AI Damage Assessment Tool</h2>
+                            <p>
+                                <b>Step 1</b>: Upload an image whose damage you want to assess. We do not store any uploaded images.<br/>
+                                <b>Step 2</b>: Click the submit button.<br/>
+                                <b>Step 3</b>: A dialog box with opens up.<br/>
+                                <b>Step 4</b>: Review the AI analysis.<br/>
+                                <b>Step 5</b>: Close the dialog box.<br/>
+                            </p>
                         </div>
                     </section>
                 </div>
