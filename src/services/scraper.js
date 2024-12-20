01bbe9eb115f083dafad6ac65346336f7b8c3d43
Mohana Misra <iammohanamisra@gmail.com>
2024-12-20 12:33:24 +0530

    Web scraping for reports working

--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
 const url = "https://reliefweb.int/node/4115495";
 
 axios(url)
 .then(response => {
     const html = response.data;
-    console.log(html);
+    const $ = cheerio.load(html);
+    const report = $("div.rw-report__content").text()
+    console.log(report);
 })
 .catch(console.error);
