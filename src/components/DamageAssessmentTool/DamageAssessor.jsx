151c052c0e3f2f2ce712d684e079173858922b71
Mohana Misra <iammohanamisra@gmail.com>
2024-12-21 20:00:16 +0530

    Refactored General Overview, Jobs section, AI Flood Predictor and Damage Assessment into their individual frontend components

---
+++ src/components/DamageAssessmentTool/DamageAssessor.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DamageAssessor = () => {
+    return (
+        <div className="ai-damage-assement">
+            <h2>AI Damage Assessment Tool</h2>
+            <p>
+                <b>Step 1</b>: Upload an image whose damage you want to assess. We do not store any uploaded
+                images.<br/>
+                <b>Step 2</b>: Click the submit button.<br/>
+                <b>Step 3</b>: A dialog box with opens up.<br/>
+                <b>Step 4</b>: Review the AI analysis.<br/>
+                <b>Step 5</b>: Close the dialog box.<br/>
+            </p>
+        </div>
+    );
+};
+
+export default DamageAssessor;
---
+++ src/components/Jobs/Jobs.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useEffect, useState} from "react";
+import {fetchJobs} from "../../api/index.js";
+
+const Jobs = () => {
+    const [jobsList, setJobList] = useState([]);
+
+    useEffect(() => {
+        fetchJobs()
+            .then(response => {
+                const newJobsList = [];
+                response.data.data.forEach(job => {
+                    const newJob = {
+                        title: job.fields.title,
+                        source: job.fields.source[0].name,
+                        closing: job.fields.date.closing,
+                        url: job.fields.url,
+
+                    };
+                    newJobsList.push(newJob);
+                })
+                setJobList(newJobsList);
+            })
+    }, []);
+    return (
+        <ul className="jobs">
+            {jobsList.map((job, index) => (
+                <li key={index}>
+                    <span className="title">{job.title}</span>
+                    <span className="source">{job.source}</span>
+                    <span className="closing">Apply by: {job.closing}</span>
+                    <span className="url"><a href={job.url}>Link to apply</a></span>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Jobs;
