97859333540d2b76d5cb76fe2c55f0e3e33e4538
Mohana Misra <iammohanamisra@gmail.com>
2024-12-22 13:55:24 +0530

    Added styling to AI Damage Assessment Tool section

--- src/components/DamageAssessmentTool/DamageAssessor.jsx
+++ src/components/DamageAssessmentTool/DamageAssessor.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import "./DamageAssessor.css"
 
 const DamageAssessor = () => {
     return (
-        <div className="ai-damage-assement">
+        <div className="ai-damage-assessment">
             <h2>AI Damage Assessment Tool</h2>
             <p>
-                <b>Step 1</b>: Upload an image whose damage you want to assess. We do not store any uploaded
+                <span className = "label">Step 1</span>: Upload an image whose damage you want to assess. We do not store any uploaded
                 images.<br/>
-                <b>Step 2</b>: Click the submit button.<br/>
-                <b>Step 3</b>: A dialog box with opens up.<br/>
-                <b>Step 4</b>: Review the AI analysis.<br/>
-                <b>Step 5</b>: Close the dialog box.<br/>
+                <span className = "label">Step 2</span>: Click the submit button.<br/>
+                <span className= "label">Step 3</span>: A dialog box with opens up.<br/>
+                <span className= "label">Step 4</span>: Review the AI analysis.<br/>
+                <span className= "label">Step 5</span>: Close the dialog box.<br/>
             </p>
         </div>
     );
