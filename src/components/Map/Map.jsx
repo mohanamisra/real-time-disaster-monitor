34360230dc4a202ff8d502389aca542724eac5f8
Mohana Misra <iammohanamisra@gmail.com>
2024-12-23 12:19:14 +0530

    Updated Map to show India

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
 import './Map.css';
 
 const Map = () => {
-    const position = [51.505, -0.09];
+    const indiaPosition = [22.3511148, 78.6677428];
+    const zoomLevel = 5;
 
     return (
-            <MapContainer center={position} zoom={13} style={{height:'100%', width:'100%'}}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+        <MapContainer center={indiaPosition} zoom={zoomLevel} style={{ height: '100%', width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {/*<Marker position={position}>*/}
+            {/*    <Popup>*/}
+            {/*        A pretty popup. <br /> Easily customizable.*/}
+            {/*    </Popup>*/}
+            {/*</Marker>*/}
+        </MapContainer>
     );
 };
 
