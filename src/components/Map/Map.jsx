964c0ee13e3516106bfc77727e590a1d9afd5d37
Mohana Misra <iammohanamisra@gmail.com>
2024-12-23 20:23:13 +0530

    Added nearby hospital count feature

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {fetchNearbyHospitals} from "../../services/nearby.js";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -35,6 +36,9 @@ const landslideIcon = new L.Icon({
 const Map = () => {
     const indiaPosition = [22.3511148, 78.6677428];
     const zoomLevel = 5;
+    const [hospitalCount, setHospitalCount] = useState(null);
+    const [selectedLocation, setSelectedLocation] = useState(null); // Store the location of the selected marker
+
 
     const earthquakeLocations = [
         [29.5892, 79.6467, 4],
@@ -176,7 +180,18 @@ const Map = () => {
         [29.3924, 79.4534],
         [25.5488, 92.3814],
         [31.4685, 76.2708]
-]
+    ]
+
+
+    const handleMarkerClick = async (latitude, longitude) => {
+        try {
+            const count = await fetchNearbyHospitals(latitude, longitude);
+            setHospitalCount(count);
+            setSelectedLocation([latitude, longitude]);
+        } catch (error) {
+            alert('Error fetching count of nearby hospitals', error);
+        }
+    };
 
     return (
         <MapContainer center={indiaPosition} zoom={zoomLevel} style={{ height: '100%', width: '100%' }}>
@@ -185,28 +200,64 @@ const Map = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             {earthquakeLocations.map((location, i) => (
-                <Marker position={location} icon={earthquakeIcon} key={i}>
+                <Marker
+                    position={location}
+                    icon={earthquakeIcon}
+                    key={i}
+                    eventHandlers={{
+                        click: ()=>{handleMarkerClick(location[0], location[1])}
+                    }}
+                >
                     <Popup>
                         <div style={{background: 'white'}}>
-                            Earthquake Zone Classification: {location[2]}
+                            Earthquake Zone Classification: {location[2]}<br/>
+                            {selectedLocation && selectedLocation[0] === location[0] && selectedLocation[1] === location[1] && hospitalCount ? (
+                                <div className="location-info">
+                                    <span>{hospitalCount} hospital(s) nearby. Enter the location on the map to find out more.</span>
+                                </div>
+                            ) : (
+                                <span>Loading nearby hospitals...</span>
+                            )}
                         </div>
                     </Popup>
                 </Marker>
             ))}
             {cycloneLocations.map((location, i) => (
-                <Marker position={location} icon={cycloneIcon} key={i}>
+                <Marker position={location} icon={cycloneIcon} key={i}
+                        eventHandlers={{
+                            click: ()=>{handleMarkerClick(location[0], location[1])}
+                        }}
+                >
                     <Popup>
                         <div style={{background: 'white'}}>
-                            Cyclone Risk: {location[2] === 2 ? <span>High</span>:<span>Very High</span>}
+                            Cyclone Risk: {location[2] === 2 ? <span>High</span>:<span>Very High</span>}<br/>
+                            {selectedLocation && selectedLocation[0] === location[0] && selectedLocation[1] === location[1] && hospitalCount ? (
+                                <div className="location-info">
+                                    <span>{hospitalCount} hospital(s) nearby. Enter the location on the map to find out more.</span>
+                                </div>
+                            ) : (
+                                <span>Loading nearby hospitals...</span>
+                            )}
                         </div>
                     </Popup>
                 </Marker>
             ))}
             {landslideLocations.map((location, i) => (
-                <Marker position={location} icon={landslideIcon} key={i}>
+                <Marker position={location} icon={landslideIcon} key={i}
+                        eventHandlers={{
+                            click: ()=>{handleMarkerClick(location[0], location[1])}
+                        }}
+                >
                     <Popup>
                         <div style={{background: 'white'}}>
-                            Cyclone Risk: {location[2] === 2 ? <span>High</span>:<span>Very High</span>}
+                            Landslide Prone Zone<br/>
+                            {selectedLocation && selectedLocation[0] === location[0] && selectedLocation[1] === location[1] && hospitalCount ? (
+                                <div className="location-info">
+                                    <span>{hospitalCount} hospital(s) nearby. Enter the location on the map to find out more.</span>
+                                </div>
+                            ) : (
+                                <span>Loading nearby hospitals...</span>
+                            )}
                         </div>
                     </Popup>
                 </Marker>
