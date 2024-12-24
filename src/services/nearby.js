1d5ea2015c55ad8d6374a875aa6a0c177b0b69a3
Mohana Misra <iammohanamisra@gmail.com>
2024-12-24 13:28:07 +0530

    Added api call for calculating shelters with 5 km of disaster prone zones

--- src/services/nearby.js
+++ src/services/nearby.js
@@ -69,3 +69,27 @@ export const fetchAffectedSchools = async(latitude, longitude, radius=500) => {
     )
     return result.elements;
 }
+
+export const fetchNearbyShelters = async(latitude, longitude, radius=5000) => {
+    let result = await fetch(
+        "https://overpass-api.de/api/interpreter",
+        {
+            method: "POST",
+            // The body contains the query
+            // to understand the query language see "The Programmatic Query Language" on
+            // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
+            body: "data="+ encodeURIComponent(`
+            [bbox:30.618338,-96.323712,30.591028,-96.330826]
+            [out:json]
+            [timeout:90]
+            ;
+            node
+                [amenity=shelter]
+                (around:${radius}, ${latitude}, ${longitude}); // Radius in meters, then lat, lon
+            out;`)
+        },
+    ).then(
+        (data)=>data.json()
+    )
+    return result.elements;
+}
