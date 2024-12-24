bbb90190674093194abda511dec79bf467c39d24
Mohana Misra <iammohanamisra@gmail.com>
2024-12-24 13:22:59 +0530

    Added functionality of finding out total number of hospitals and schools prone to damages due to being within a 500 metre radius of disaster-prone zones

--- src/services/nearby.js
+++ src/services/nearby.js
@@ -21,3 +21,51 @@ export const fetchNearbyHospitals = async(latitude, longitude, radius=5000) => {
     )
     return result.elements.length;
 }
+
+export const fetchAffectedHospitals = async(latitude, longitude, radius=500) => {
+    let result = await fetch(
+        "https://overpass-api.de/api/interpreter",
+        {
+            method: "POST",
+            // The body contains the query
+            // to understand the query language see "The Programmatic Query Language" on
+            // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
+            body: "data="+ encodeURIComponent(`
+            [bbox:30.618338,-96.323712,30.591028,-96.330826]
+            [out:json]
+            [timeout:90]
+            ;
+            node
+                [amenity=hospital]
+                (around:${radius}, ${latitude}, ${longitude}); // Radius in meters, then lat, lon
+            out;`)
+        },
+    ).then(
+        (data)=>data.json()
+    )
+    return result.elements;
+}
+
+export const fetchAffectedSchools = async(latitude, longitude, radius=500) => {
+    let result = await fetch(
+        "https://overpass-api.de/api/interpreter",
+        {
+            method: "POST",
+            // The body contains the query
+            // to understand the query language see "The Programmatic Query Language" on
+            // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
+            body: "data="+ encodeURIComponent(`
+            [bbox:30.618338,-96.323712,30.591028,-96.330826]
+            [out:json]
+            [timeout:90]
+            ;
+            node
+                [amenity=school]
+                (around:${radius}, ${latitude}, ${longitude}); // Radius in meters, then lat, lon
+            out;`)
+        },
+    ).then(
+        (data)=>data.json()
+    )
+    return result.elements;
+}
