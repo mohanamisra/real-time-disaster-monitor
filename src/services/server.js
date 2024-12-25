1943374db06ca47ba63d45ad8ceb31e0a60f8955
Mohana Misra <iammohanamisra@gmail.com>
2024-12-25 22:44:29 +0530

    Heatwave Predictor working as expected

--- src/services/server.js
+++ src/services/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 import { fetchReports } from './scraper.js'; // Adjust the path if needed
 //
-// const app = express();
+const app = express();
 // const port = 3000;
-// app.use(cors());
+app.use(cors());
 //
 // app.get('/fetch-report', async (req, res) => {
 //     const url = req.query.url; // URL sent as a query parameter
@@ -26,19 +29,44 @@ import { fetchReports } from './scraper.js'; // Adjust the path if needed
 //     console.log(`App listening at http://localhost:${port}`);
 // });
 
-const app = express();
+app.get('/location', async (req, res) => {
+    const { locationName } = req.query;
+    if (!locationName) {
+        return res.status(400).json({ error: 'Location name is required.' });
+    }
 
-app.use(cors());
-
-app.get('/elevation', async (req, res) => {
     try {
-        const response = await axios.get(
-            'https://api.opentopodata.org/v1/srtm30m?locations=57.688709,11.976404'
-        );
+        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${locationName},IN&limit=5&appid=${process.env.OPENWEATHER_API_KEY}`);
         res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
+        res.status(500).json({ error: 'Failed to fetch location data' });
     }
 });
+
+app.get('/weather', async(req, res) => {
+    const { lat, long } = req.query;
+    if(!lat || !long) {
+        return res.status(400).json({error: 'Latitude and Longitude are required.'});
+    }
+    try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
+        res.json(response.data);
+    } catch(error) {
+        console.error(error.response.data);
+    }
+})
+
+app.get('/elevation', async (req, res) => {
+    const { lat, long } = req.query;
+    if(!lat || !long) {
+        return res.status(400).json({error: 'Latitude and Longitude are required.'});
+    }
+    try {
+        const response = await axios.get(`https://api.opentopodata.org/v1/srtm90m?locations=${lat},${long}`);
+        res.json(response.data);
+    }catch(error) {
+        console.error(error.response.data);
+    }
+})
 
 app.listen(3000, () => console.log('Server running on port 3000'));
