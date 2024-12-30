b3246fe9e20419468a207070a18f0c3812bbed15
Mohana Misra <iammohanamisra@gmail.com>
2024-12-31 02:07:25 +0530

    Revert "Removed unused scraper files, report api"
    This reverts commit 9bdf7b91d0a931cf8db019be65532fd165dd026c.

--- src/services/server.js
+++ src/services/server.js
@@ -5,8 +5,30 @@ import dotenv from 'dotenv';
 import { onRequest } from "firebase-functions/v2/https";
 
 dotenv.config();
+import { fetchReports } from './scraper.js'; // Adjust the path if needed
+//
 const app = express();
+// const port = 3000;
 app.use(cors());
+//
+// app.get('/fetch-report', async (req, res) => {
+//     const url = req.query.url; // URL sent as a query parameter
+//     if (!url) {
+//         return res.status(400).send({ error: 'URL parameter is required' });
+//     }
+//
+//     try {
+//         const report = await fetchReports(url);
+//         res.send({ report });
+//     } catch (error) {
+//         console.error("Error fetching report:", error);
+//         res.status(500).send({ error: 'Failed to fetch the report' });
+//     }
+// });
+//
+// app.listen(port, () => {
+//     console.log(`App listening at http://localhost:${port}`);
+// });
 
 app.get('/api/location', async (req, res) => {
     const { locationName } = req.query;
