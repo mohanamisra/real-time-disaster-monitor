00133b7a244431870c22b06296e98f1c1ca9555a
Mohana Misra <iammohanamisra@gmail.com>
2024-12-31 02:43:33 +0530

    Refactored code to redirect to backend on Render

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://real-time-disaster-monitoring-backend.onrender.com'; // Production URL
+
 export const fetchLocationCoords = async (locationName) => {
     console.log(locationName);
-    return await axios.get(`http://localhost:3000/location`, {
+    return await axios.get(`${BASE_URL}/location`, {
         params: { locationName },
     });
 };
 
 export const fetchWeather = async (lat, long) => {
-    return await axios.get(`http://localhost:3000/weather`, {
-        params: {lat, long},
+    return await axios.get(`${BASE_URL}/weather`, {
+        params: { lat, long },
     });
 };
 
-export const fetchHeatWaveReport = async(lat, long) => {
-    return await axios.get(`http://localhost:3000/elevation`, {
-        params: {lat, long},
+export const fetchHeatWaveReport = async (lat, long) => {
+    return await axios.get(`${BASE_URL}/elevation`, {
+        params: { lat, long },
     });
 };
